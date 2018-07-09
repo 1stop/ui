fc4b1b646c5726309337226e9ac06541852c7946
Lee Yeong Khang <sindrosa.24@gmail.com>
2018-07-09 21:35:21 +0800

    autofocus on init

--- src/app/element/search-bar/search-bar.component.ts
+++ src/app/element/search-bar/search-bar.component.ts
@@ -1,14 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, OnInit, ElementRef, Input } from '@angular/core';
 
 @Component({
     selector: 'app-search-bar',
     templateUrl: './search-bar.component.html',
     styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
     focus = false;
     query = '';
+    @Input() shouldFocus = false;
     @Output() search: EventEmitter<string> = new EventEmitter();
+    @ViewChild('input') searchbar: ElementRef;
+
+    ngOnInit() {
+        if ( this.shouldFocus ) {
+            this.searchbar.nativeElement.focus();
+        }
+    }
 
     commit() {
         this.search.emit(this.query);
