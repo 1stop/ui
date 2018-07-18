938da61e3393ef2ef16cd1c92ee8a7f213d0a243
DESKTOP-J41QGHN\Yeong <greatspirit828@gmail.com>
2018-07-18 20:10:51 +0800

    Hack method in order for the editor display normally

--- src/app/element/text-editor/text-editor.component.ts
+++ src/app/element/text-editor/text-editor.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, OnInit, ElementRef } from '@angular/core';
 
-import '../../../public/vendor/codemirror/codemirror.css'; // codemirror
+// import '../../../public/vendor/codemirror/codemirror.css'; // codemirror
 import 'tui-editor/dist/tui-editor.css'; // editor ui
 import 'tui-editor/dist/tui-editor-contents.css'; // editor content
-import '../../../public/vendor/highlight.js/github.css'; // code block highlight
+// import '../../../public/vendor/highlight.js/github.css'; // code block highlight
 import 'tui-editor/dist/tui-editor-extTable.js';
 
 import Editor from 'tui-editor/dist/tui-editor-Editor';
@@ -11,10 +11,10 @@ import Editor from 'tui-editor/dist/tui-editor-Editor';
 @Component({
     selector: 'text-editor',
     templateUrl: './text-editor.component.html',
-    styleUrls: ['./text-editor.component.css']
+    styleUrls: ['./text-editor.component.css', './tui-editor-contents.css', './tui-editor.css']
 })
 export class TextEditorComponent implements OnInit {
-
+    
     @Input() code;
 
     editor: any;
@@ -30,7 +30,7 @@ export class TextEditorComponent implements OnInit {
             height: 'auto',
             initialValue: this.code,
             hideModeSwitch: true,
-            viewer: true,
+            viewer: false,
             exts: ['table']
         });
     }
