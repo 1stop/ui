d4ece58ccc648fcccb274ef60b9c1b79475fbdf8
Lee Yeong Khang <sindrosa.24@gmail.com>
2018-08-02 21:57:44 +0800

    fix login without using firebase-ui

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Component, OnInit } from '@angular/core';
-import * as firebaseui from 'firebaseui';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
@@ -7,38 +6,15 @@ import 'firebaseui/dist/firebaseui.css';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { BehaviorSubject, ReplaySubject } from 'rxjs';
 
-const UI_CONFIG = {
-    signInSuccessUrl: '/',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
-    // Terms of service url.
-    tosUrl: 'https://en.wikipedia.org/wiki/Terms_of_service',
-    privacyPolicyUrl: 'https://www.google.com'
-  };
-
-
 @Injectable()
 export class UserService {
     user: ReplaySubject<any> = new ReplaySubject<any>(1);
     authUI: any;
     constructor(public dialog: MatDialog,
                 public afAuth: AngularFireAuth) {
-        this.authUI = new firebaseui.auth.AuthUI(firebase.auth());
         this.afAuth.auth.onAuthStateChanged((user) => {
             this.user.next(user);
         });
-        this.user.subscribe((v) => { console.log(v); });
-
-        (<any>window).login = () => {
-            this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider()).then((v) => {
-                console.log('logged in');
-            });
-
-        };
     }
 
     login() {
@@ -49,14 +25,52 @@ export class UserService {
 
 @Component({
     selector: 'app-login-dialog',
-    template: `<div id="firebaseui-auth-container"></div>`,
+    template: `<div id="firebaseui-auth-container">
+    <div id="firebaseui-auth-container" lang="en">
+        <div class="firebaseui-container firebaseui-page-provider-sign-in firebaseui-id-page-provider-sign-in">
+            <div class="firebaseui-card-content">
+                <form onsubmit="return false;">
+                    <ul class="firebaseui-idp-list">
+                        <li class="firebaseui-list-item">
+                            <button (click)="login('google')" class="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-google firebaseui-id-idp-button" data-provider-id="google.com" data-upgraded=",MaterialButton"><span class="firebaseui-idp-icon-wrapper"><img class="firebaseui-idp-icon" alt="" src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"></span><span class="firebaseui-idp-text firebaseui-idp-text-long">Sign in with Google</span><span class="firebaseui-idp-text firebaseui-idp-text-short">Google</span></button>
+                        </li>
+                        <li class="firebaseui-list-item">
+                            <button (click)="login('facebook')" class="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-facebook firebaseui-id-idp-button" data-provider-id="facebook.com" data-upgraded=",MaterialButton"><span class="firebaseui-idp-icon-wrapper"><img class="firebaseui-idp-icon" alt="" src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/facebook.svg"></span><span class="firebaseui-idp-text firebaseui-idp-text-long">Sign in with Facebook</span><span class="firebaseui-idp-text firebaseui-idp-text-short">Facebook</span></button>
+                        </li>
+                        <li class="firebaseui-list-item">
+                            <button (click)="login('email')" class="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-password firebaseui-id-idp-button" data-provider-id="password" data-upgraded=",MaterialButton"><span class="firebaseui-idp-icon-wrapper"><img class="firebaseui-idp-icon" alt="" src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/mail.svg"></span><span class="firebaseui-idp-text firebaseui-idp-text-long">Sign in with email</span><span class="firebaseui-idp-text firebaseui-idp-text-short">Email</span></button>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+            <div class="firebaseui-card-footer firebaseui-provider-sign-in-footer">
+                <p class="firebaseui-tos firebaseui-tospp-full-message">By continuing, you are indicating that you accept our <a href="https://en.wikipedia.org/wiki/Terms_of_service" class="firebaseui-link firebaseui-tos-link" target="_blank">Terms of Service</a> and <a href="https://www.google.com" class="firebaseui-link firebaseui-pp-link" target="_blank">Privacy Policy</a>.</p>
+            </div>
+        </div>
+    </div>
+</div>`,
 })
-export class LoginDialogComponent implements OnInit {
+export class LoginDialogComponent {
     constructor(
         public dialogRef: MatDialogRef<LoginDialogComponent>,
-        private _user: UserService) {}
+        public afAuth: AngularFireAuth) {}
+    login(provider: string) {
+        switch (provider) {
+            case 'google':
+                this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(() => {
+                    this.dialogRef.close();
+                });
+                break;
+            case 'facebook':
+                this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(() => {
+                    this.dialogRef.close();
+                });
+                break;
+            case 'email':
+                this.afAuth.auth.signInWithPopup(new firebase.auth.EmailAuthProvider()).then(() => {
+                    this.dialogRef.close();
+                });
+        }
 
-    ngOnInit() {
-        this._user.authUI.start('#firebaseui-auth-container', UI_CONFIG);
     }
 }
