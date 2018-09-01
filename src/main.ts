a331b78c3b5dfc7ab3f6e0d5f538d3a60b438a24
Lee Yeong Khang <sindrosa.24@gmail.com>
2018-09-01 21:47:27 +0800

    fix bug where hammerjs pervent text selection

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'hammerjs';
+declare var Hammer: any;
+delete Hammer.defaults.cssProps.userSelect;
 
 if (environment.production) {
   enableProdMode();
